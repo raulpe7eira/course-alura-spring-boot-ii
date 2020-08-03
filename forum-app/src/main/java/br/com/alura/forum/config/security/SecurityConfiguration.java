41ccdbac4713b47a7be5d7385e129c19dae3aad6
Raul Pereira <raul.pereira@gmail.com>
2020-08-02 23:05:09 -0300

    class 07 - setup swagger authorization header

--- forum-app/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum-app/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -54,7 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
 }
